06bd916e787b657f52783b9ddd9b26b0a324b403
jy-23 <jychung1@my.scccd.edu>
2023-05-07 22:36:55 -0700

    jeopardy added

--- volleyball.h
+++ volleyball.h
@@ -13,12 +13,20 @@ void print_dummy_question() {
 	std::cout << "   d.) 8\n" << std::endl;
 }
 
-bool volleyball(double &time_to_beat) {
+bool volleyball(double &time_to_beat, std::vector<trivia_question> &vec) {
 	auto start = std::chrono::steady_clock::now();
-	print_dummy_question();
+	//print_dummy_question();
+	int question_num = rand() % vec.size();
+	trivia_question current = vec.at(question_num);
+	current.print_question();
+
 	std::cout << "Time to beat: " << time_to_beat << " seconds" << std::endl;
 	char player_answer = read();
-	if (player_answer == 'c') {
+	while (!(player_answer > 96 && player_answer < 101) && !(player_answer > 64 && player_answer < 69)) {
+		std::cout << "Please enter a,b,c, or d." << std::endl;
+		player_answer = read();
+	}
+	if (current.check_answer(player_answer)) {
 		auto end = std::chrono::steady_clock::now();
 		double elapsed_time = std::chrono::duration<double>(end - start).count(); 
 		if (elapsed_time < time_to_beat) {
