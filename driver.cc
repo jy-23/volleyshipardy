6000f1035fc9230454024328c40c13ac67227541
Stephen200286 <vacuumman117@gmail.com>
2023-05-09 16:01:31 -0700

    Added battleship

---
+++ driver.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "battleship.h"
+using namespace std;
+
+int main() {
+	initializeP1();
+	initializeP2();
+	p1Turn();
+	p1Turn();
+	p1Turn();
+	p2Turn();
+	p2Turn();
+	p2Turn();
+}
